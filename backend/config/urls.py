7afe41a74b8f5744839be048e0e64e901ce0e1f6
Mouhamed <m3dev4@gmail.com>
2026-09-12 02:27:02 +0000

    Ajout de la configuration de base pour le projet Django, y compris les fichiers de configuration, les modèles, les vues, et les fichiers de migration pour l'application User, ainsi que les fichiers de configuration ASGI, WSGI et les paramètres du projet.

---
+++ backend/config/urls.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("sentry-debug/", trigger_error),
+    # Auth JWT
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Documentation API
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
